1604a2db50f5d6d6bf0f766e57775520f07ec3c8
Eric Liu <liuxue1990@gmail.com>
2014-10-15 17:08:29 -0500

    finish project setup and finish makefile, working version of blinky using external clock

--- inc/lab04_tasks.h
+++ inc/lab04_tasks.h
@@ -32,7 +32,7 @@ unsigned char refreshSensorData(void);
 unsigned char calculateOrientation(void); 
 
 /* 
- * This function needs to be executed once every second.    Priority 4.
+ * This function needs to be executed once every second(1000 ms).    Priority 4.
  * This function will return a struct with on-off settings for each motor.  Since the PWM's can
  * be configured in different ways the speed will be either 0 or 1 (off or on).
  */
